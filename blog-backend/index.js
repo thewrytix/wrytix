5da66534658de059d41d3b7f948d99c9dbab967d
BobRoyler <54759327+bobroyler@users.noreply.github.com>
2025-08-22 06:03:40 +0000

    https://github.com/FervTech/wrytix.git

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -32,15 +32,14 @@ app.get("/", (req, res) => {
 
 
 app.use(session({
-    secret: 'your_secret_key',
+    secret: process.env.SESSION_SECRET || 'your_secret_key', // Use env variable
     resave: false,
     saveUninitialized: false,
     cookie: {
         httpOnly: true,
-        secure: true, // since you’re on HTTPS
-        sameSite: "none",
-        maxAge: 24 * 60 * 60 * 1000
-    }
+        secure: true,
+        sameSite: 'none',
+        maxAge: 24 * 60 * 60 * 1000}
 }));
 
 app.use(helmet());
