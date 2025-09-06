12166d2cd35a2c0fb5489afc7a34596493a429c4
BobRoyler <54759327+bobroyler@users.noreply.github.com>
2025-09-06 18:26:07 +0000

    https://github.com/FervTech/wrytix .git

--- blog-backend/scripts/fix-post-ids.js
+++ blog-backend/scripts/fix-post-ids.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const { v4: uuidv4 } = require('uuid');
 
-// Replace with your actual MongoDB connection string
 const mongoURI = 'mongodb+srv://wrytix_admin:Kylerlee149143123.@cluster0.jorn0pz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; // e.g., 'mongodb://localhost:27017/test' or your Render MongoDB URI
+
+const { v4: uuidv4 } = require('uuid');
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(async () => {
