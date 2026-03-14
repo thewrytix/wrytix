2db81b412a5a33c723b42c98b5a1ddc14f168b5e
Faisel-ali <161211192+Faisel-ali@users.noreply.github.com>
2026-03-14 15:06:59 +0000

    Changed to showSuccess

--- public/backend-panel/backend-panelJS/postContent.js
+++ public/backend-panel/backend-panelJS/postContent.js
@@ -186,7 +186,7 @@ class RichTextEditor {
                 const imageUrl = await this.readFileAsDataURL(file);
                 this.insertImageAtCursor(imageUrl);
                 e.target.value = "";
-                this.showSuccess('Image added');
+               showSuccess('Image added');
             } catch (error) {
                 console.error('Image upload error:', error);
             }
