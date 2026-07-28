07110387fed391173b25706ebb9c0c1b5db9e79c
Faisel-ali <161211192+Faisel-ali@users.noreply.github.com>
2026-07-28 09:35:23 +0000

    Updating post model

--- blog-backend/routes/headline.js
+++ blog-backend/routes/headline.js
@@ -4,6 +4,7 @@ const { requireAdmin } = require('../middleware/auth');
 
 const router = express.Router();
 
+
 router.get('/headline', getHeadline);
 router.put('/headline', requireAdmin, updateHeadline);
 
